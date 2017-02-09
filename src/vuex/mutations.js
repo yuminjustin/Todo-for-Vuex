639ab6986a2486d39235e44d6826a8b6b86761fb
justin lee <yuminjustin@163.com>
2017-02-09 16:14:55 +0800

    逻辑处理函数

---
+++ src/vuex/mutations.js
@@ -0,0 +1,25 @@
+export default {
+	del:(state,idx)=>state.todos.splice(idx,1),
+	add(state,txt){
+		state.id++
+		state.todos.push({
+		  text:txt,
+		  selected:!1,
+		  id:state.id
+	   })
+	},
+	update:(state,arr)=>state.todos[arr[0]].text = arr[1],
+	select:(state,arr)=>state.todos[arr[0]].selected = arr[1],
+	changeFilter:(state,id)=>state.filter = idx,
+	delSelected(state){
+		state.todos = state.todos.filter((todo)=>{
+			return !todo.selected
+		})
+	},
+	selectAll(state,bool){
+		state.todos = state.todos.map((todo)=>{
+			todo.selected = bool
+			return todo
+		})
+	}
+}
