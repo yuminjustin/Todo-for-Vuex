fa610be36f38d18a724b91d79b559da21e93003e
justin lee <yuminjustin@163.com>
2017-02-09 16:11:59 +0800

    todos主体列表

---
+++ src/components/list.vue
@@ -0,0 +1,16 @@
+<template>
+   <div class="c oh listbox">
+	  <ul>
+	    <li is = "Item" v-for="todo in $store.state.todos" :todo="todo">
+		</li>
+	  </ul>
+  </div>
+</template>
+<script>
+  import Item from './item'
+  export default {
+	 components:{
+	   Item
+	 }
+  }
+</script>
