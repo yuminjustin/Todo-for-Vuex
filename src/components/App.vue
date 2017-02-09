615b982ca85f4bd8b963f86fffdd818ab3f6f514
justin lee <yuminjustin@163.com>
2017-02-09 16:10:29 +0800

    app主文件

---
+++ src/components/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mainbox m oh">
+	<h1>{{title}}</h1>
+	<InputBox/>
+	<List/>
+	<FootBar/>
+  </div>
+</template>
+
+<script>
+import InputBox from './input'
+import List from './list'
+import FootBar from './foot'
+
+export default {
+   data(){
+      return {
+	     title:"Vuex todo"
+	  }
+   },
+   components:{
+      InputBox,
+	  List,
+	  FootBar
+   }
+}
+</script>
